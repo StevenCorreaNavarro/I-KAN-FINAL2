3bf8c1f59b330fe7c2b8c827318c4fddbfea443e
LuisIrua23 <lairua@soy.sena.edu.co>
2024-10-09 23:19:20 -0500

    api3

--- resources/views/estilo_casual.blade.php
+++ resources/views/estilo_casual.blade.php
@@ -54,7 +54,7 @@
 
     <script>
         function fetchSearchResults() {
-            const apiKey = 'AIzaSyANf_BSS299tblgVNNbB_gZYjGgcuO5qLg';
+            const apiKey = 'AIzaSyDnbP4Uhy8MPMtLzaugfqOrJl0vsdfdb_Y';
             const searchEngineId = 'b751aa8b758cc4d6a';
             const queries = ['blazers', 'jeans', 'gafas', 'camisas', 'botas', 'Blusas*','abrigos','botines','gaban',
             '']; // Lista de términos de búsqueda
